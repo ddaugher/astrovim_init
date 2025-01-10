15b7beb7d13dbeae8ba061245bd945692e727eb4
DJ Daugherty <ddaugher@gmail.com>
2025-01-10 18:24:21 -0500

    fix: updates to setup

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -24,12 +24,12 @@ return {
 		},
 		-- vim options can be configured here
 		options = {
-			opt = {              -- vim.opt.<key>
-				relativenumber = true, -- sets vim.opt.relativenumber
-				number = true,     -- sets vim.opt.number
-				spell = false,     -- sets vim.opt.spell
+			opt = {               -- vim.opt.<key>
+				relativenumber = false, -- sets vim.opt.relativenumber
+				number = true,      -- sets vim.opt.number
+				spell = false,      -- sets vim.opt.spell
 				signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-				wrap = false,      -- sets vim.opt.wrap
+				wrap = false,       -- sets vim.opt.wrap
 			},
 			o = {
 				hlsearch = true,
