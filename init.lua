d958c882e08497683e13af6efec5c05a372bd00d
DJ Daugherty <ddaugher@gmail.com>
2025-01-13 09:58:20 -0500

    fix: updates

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ vim.opt.rtp:prepend(lazypath)
 if not pcall(require, "lazy") then
   -- stylua: ignore
   vim.api.nvim_echo(
-  { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
+    { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
     true, {})
   vim.api.nvim_create_autocmd("VimLeavePre", {
     pattern = "*",
