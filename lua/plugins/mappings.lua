0d72e89a290fa69aea2f9deb48a0ef945f5d5c28
DJ Daugherty <ddaugher@gmail.com>
2025-01-12 19:35:02 -0500

    fix: more updates

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -12,11 +12,15 @@ return {
           ["<C-s>"] = { ":w!<cr>", desc = "Save File" },
           -- navigate buffer tabs with `H` and `L`
           L = {
-            function() require("astrocore.buffer").nav(vim.v.count1) end,
+            function()
+              require("astrocore.buffer").nav(vim.v.count1)
+            end,
             desc = "Next buffer",
           },
           H = {
-            function() require("astrocore.buffer").nav(-vim.v.count1) end,
+            function()
+              require("astrocore.buffer").nav(-vim.v.count1)
+            end,
             desc = "Previous buffer",
           },
           -- tables with just a `desc` key will be registered with which-key if it's installed
@@ -25,9 +29,9 @@ return {
           ["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
           ["<Leader>bD"] = {
             function()
-              require("astroui.status").heirline.buffer_picker(
-                function(bufnr) require("astrocore.buffer").close(bufnr) end
-              )
+              require("astroui.status").heirline.buffer_picker(function(bufnr)
+                require("astrocore.buffer").close(bufnr)
+              end)
             end,
             desc = "Pick to close",
           },
