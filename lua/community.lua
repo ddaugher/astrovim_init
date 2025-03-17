acd1a7a09414c9950a9d8e2dfe62ed24694a863f
DJ Daugherty <ddaugher@gmail.com>
2025-03-16 20:34:55 -0400

    fix: adding mini.ai

--- lua/community.lua
+++ lua/community.lua
@@ -5,16 +5,17 @@
 ---@type LazySpec
 return {
 	"AstroNvim/astrocommunity",
-	{ import = "astrocommunity.colorscheme.nord-nvim" },           -- import/override with your plugins folder
-	{ import = "astrocommunity.completion.cmp-nvim-lua" },         -- import/override with your plugins folder
-	{ import = "astrocommunity.completion.copilot-lua-cmp" },      -- import/override with your plugins folder
-	{ import = "astrocommunity.editing-support.auto-save-nvim" },  -- import/override with your plugins folder
+	{ import = "astrocommunity.colorscheme.nord-nvim" }, -- import/override with your plugins folder
+	{ import = "astrocommunity.completion.cmp-nvim-lua" }, -- import/override with your plugins folder
+	{ import = "astrocommunity.completion.copilot-lua-cmp" }, -- import/override with your plugins folder
+	{ import = "astrocommunity.editing-support.auto-save-nvim" }, -- import/override with your plugins folder
 	{ import = "astrocommunity.editing-support.copilotchat-nvim" }, -- import/override with your plugins folder
 	{ import = "astrocommunity.editing-support.vim-visual-multi" },
 	{ import = "astrocommunity.pack.elixir-phoenix" },
 	{ import = "astrocommunity.pack.lua" },
-	{ import = "astrocommunity.search.nvim-spectre" },                    -- import/override with your plugins folder
+	{ import = "astrocommunity.search.nvim-spectre" }, -- import/override with your plugins folder
 	{ import = "astrocommunity.terminal-integration.vim-tmux-navigator" }, -- import/override with your plugins folder
 	{ import = "astrocommunity.test.vim-test" },
-	{ import = "astrocommunity.utility.hover-nvim" },                     -- import/override with your plugins folder
+	{ import = "astrocommunity.utility.hover-nvim" }, -- import/override with your plugins folder
+	{ import = "astrocommunity.motion.mini-ai" }, -- import/override with your plugins folder
 }
