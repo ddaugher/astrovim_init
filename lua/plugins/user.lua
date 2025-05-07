1708c9e92c640945415486bf91414a617d6cb7cf
DJ Daugherty <ddaugher@gmail.com>
2025-05-07 00:04:25 -0400

    fix: upgrading to v5

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -19,22 +19,35 @@ return {
 
 	-- == Examples of Overriding Plugins ==
 
-	-- customize alpha options
 	{
-		"goolord/alpha-nvim",
-		opts = function(_, opts)
-			-- customize the dashboard header
-			opts.section.header.val = {
-				" █████╗ ██╗   ██╗ ██████╗ ██╗   ██╗███████╗████████╗██╗    ██╗███████╗███╗   ██╗████████╗██╗   ██╗",
-				"██╔══██╗██║   ██║██╔════╝ ██║   ██║██╔════╝╚══██╔══╝██║    ██║██╔════╝████╗  ██║╚══██╔══╝╚██╗ ██╔╝",
-				"███████║██║   ██║██║  ███╗██║   ██║███████╗   ██║   ██║ █╗ ██║█████╗  ██╔██╗ ██║   ██║    ╚████╔╝ ",
-				"██╔══██║██║   ██║██║   ██║██║   ██║╚════██║   ██║   ██║███╗██║██╔══╝  ██║╚██╗██║   ██║     ╚██╔╝  ",
-				"██║  ██║╚██████╔╝╚██████╔╝╚██████╔╝███████║   ██║   ╚███╔███╔╝███████╗██║ ╚████║   ██║      ██║   ",
-				"╚═╝  ╚═╝ ╚═════╝  ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝    ╚══╝╚══╝ ╚══════╝╚═╝  ╚═══╝   ╚═╝      ╚═╝   ",
-			}
-			return opts
-		end,
+		"folke/snacks.nvim",
+		---@type snacks.Config
+		opts = {
+			dashboard = {
+				enabled = true,
+				preset = {
+
+					header = [[
+					 █████╗ ██╗   ██╗ ██████╗ ██╗   ██╗███████╗████████╗██╗    ██╗███████╗███╗   ██╗████████╗██╗   ██╗
+					██╔══██╗██║   ██║██╔════╝ ██║   ██║██╔════╝╚══██╔══╝██║    ██║██╔════╝████╗  ██║╚══██╔══╝╚██╗ ██╔╝
+					███████║██║   ██║██║  ███╗██║   ██║███████╗   ██║   ██║ █╗ ██║█████╗  ██╔██╗ ██║   ██║    ╚████╔╝
+					██╔══██║██║   ██║██║   ██║██║   ██║╚════██║   ██║   ██║███╗██║██╔══╝  ██║╚██╗██║   ██║     ╚██╔╝
+					██║  ██║╚██████╔╝╚██████╔╝╚██████╔╝███████║   ██║   ╚███╔███╔╝███████╗██║ ╚████║   ██║      ██║
+					╚═╝  ╚═╝ ╚═════╝  ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝    ╚══╝╚══╝ ╚══════╝╚═╝  ╚═══╝   ╚═╝      ╚═╝
+				]],
+				},
+			},
+		},
 	},
+
+	-- -- customize alpha options
+	-- {
+	-- 	"goolord/alpha-nvim",
+	-- 	opts = function(_, opts)
+	-- 		-- customize the dashboard header
+	-- 		return opts
+	-- 	end,
+	-- },
 
 	-- You can disable default plugins as follows:
 	{ "max397574/better-escape.nvim", enabled = false },
@@ -50,18 +63,18 @@ return {
 			npairs.add_rules(
 				{
 					Rule("$", "$", { "tex", "latex" })
-					-- don't add a pair if the next character is %
-							:with_pair(cond.not_after_regex("%%"))
-					-- don't add a pair if  the previous character is xxx
-							:with_pair(
-								cond.not_before_regex("xxx", 3)
-							)
-					-- don't move right when repeat character
-							:with_move(cond.none())
-					-- don't delete if the next character is xx
-							:with_del(cond.not_after_regex("xx"))
-					-- disable adding a newline when you press <cr>
-							:with_cr(cond.none()),
+						-- don't add a pair if the next character is %
+						:with_pair(cond.not_after_regex("%%"))
+						-- don't add a pair if  the previous character is xxx
+						:with_pair(
+							cond.not_before_regex("xxx", 3)
+						)
+						-- don't move right when repeat character
+						:with_move(cond.none())
+						-- don't delete if the next character is xx
+						:with_del(cond.not_after_regex("xx"))
+						-- disable adding a newline when you press <cr>
+						:with_cr(cond.none()),
 				},
 				-- disable for .vim files, but it work for another filetypes
 				Rule("a", "a", "-vim")
